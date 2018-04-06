4de897cbed2bf98b38d89ef37fd0e8ffb66f26b5
wabywang <wabywang@tencent.com>
2018-04-06 14:38:59 +0800

    supoort optional gpu nums

--- search.sh
+++ search.sh
@@ -1,18 +1,19 @@
 echo "use gpu with multiple processes";
 #list="SST_2 SST_5 MPQA TREC SUBJ MR CR"
 list="SST_5 MPQA TREC SUBJ MR CR"
-count = 7
+
+declare -i count=2
+echo $count
 for dataset in $list
 do
     echo process data $dataset 
-    for((i=0;i<$count;i++))
+    for((i=1;i<= $count;i++))
     do
         {
          echo "use gpu" +$i ; 
          echo CUDA_VISIBLE_DEVICES=$i python multi_search.py -gpu $i -dataset $dataset -count $count; 
-         CUDA_VISIBLE_DEVICES=$i python multi_search.py -gpu $i -dataset $dataset -count $count; 
-         
-         }&
+         CUDA_VISIBLE_DEVICES=$i python multi_search.py -gpu $i -dataset $dataset -count $count;         
+        }&
     done
     wait
     echo finished with dataset: $dataset 
