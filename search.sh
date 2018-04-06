96ea440af96cfeefc44c9deb193e145d3ed5386f
wabywang <wabywang@tencent.com>
2018-04-06 12:36:34 +0800

    support many datasets

--- search.sh
+++ search.sh
@@ -1,11 +1,18 @@
 echo "use gpu with multiple processes";
-for((i=0;i<=7;i++))
+#list="SST_2 SST_5 MPQA TREC SUBJ MR CR"
+list="SST_5 MPQA TREC SUBJ MR CR"
+for dataset in $list
 do
-    {
-     echo "use gpu" +$i ; 
-     echo CUDA_VISIBLE_DEVICES=$i python multi_search.py -gpu $i; 
-     CUDA_VISIBLE_DEVICES=$i python multi_search.py -gpu $i; 
-     
-     }&
+    echo process data $dataset 
+    for((i=0;i<=7;i++))
+    do
+        {
+         echo "use gpu" +$i ; 
+         echo CUDA_VISIBLE_DEVICES=$i python multi_search.py -gpu $i -dataset $dataset; 
+         CUDA_VISIBLE_DEVICES=$i python multi_search.py -gpu $i -dataset $dataset; 
+         
+         }&
+    done
+    wait
+    echo finished with dataset: $dataset 
 done
-wait
